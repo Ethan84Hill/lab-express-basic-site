98b1c911ad8988c49f81b497599eb5284f9cf59b
Ethan <ethan84hill@gmail.com>
2022-11-08 17:00:28 -0500

    made additional changes

--- app.js
+++ app.js
@@ -2,26 +2,26 @@ const express = require('express');
 
 const app = express();
 
-// app.use(express.static('public'))
+ app.use(express.static('public'))
 
 app.get('/home', (request, response, next) => {
     console.log(request)
-    response.sendFile(__dirname + 'views/home-page.html');
+    response.sendFile(__dirname + '/views/home-page.html');
 });
 
 app.get('/about', (request, response, next) => {
     console.log(request)
-    response.sendFile(__dirname + 'views/about-page.html');
+    response.sendFile(__dirname + '/views/about-page.html');
 });
 
 app.get('/works', (request, response, next) => {
     console.log(request)
-    response.sendFile(__dirname + 'views/works-page.html');
+    response.sendFile(__dirname + '/views/works-page.html');
 });
 
 app.get('/photos', (request, response, next) => {
     console.log(request)
-    response.sendFile(__dirname + 'views/photos-page.html');
+    response.sendFile(__dirname + '/views/photos-page.html');
 });
 
 app.listen(3000, () => { console.log('server is live') })
